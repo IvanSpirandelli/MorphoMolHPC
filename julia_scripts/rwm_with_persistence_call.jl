a8532a1c54d4f522c21d19cbc99948ddcc4e3a4b
Ivan Spirandelli <ivan@spirandelli.de>
2024-10-23 16:48:26 -0700

    adjusting temperatures

--- julia_scripts/rwm_with_persistence_call.jl
+++ julia_scripts/rwm_with_persistence_call.jl
@@ -26,7 +26,7 @@ function rwm_with_persistence_call(
     pf = MorphoMol.Energies.get_prefactors(rs, η)
     Σ = vcat([[σ_r, σ_r, σ_r, σ_t, σ_t, σ_t] for _ in 1:n_mol]...)
 
-    energy(x) = solvation_free_energy_with_persistence_and_measures_in_bounds(x, template_centers, radii, rs, pf, 0.0, overlap_slope, persistence_weights, bnds, delaunay_eps)
+    energy(x) = solvation_free_energy_with_persistence_in_bounds_without_diagrams(x, template_centers, radii, rs, pf, 0.0, overlap_slope, persistence_weights, bnds, delaunay_eps)
     perturbation(x) = perturb_single_randomly_chosen(x, σ_r, σ_t)
     #perturbation(x) = perturb_all(x, Σ)
 
@@ -80,12 +80,24 @@ function solvation_free_energy_with_persistence_and_measures_in_bounds(x::Vector
     if any(0.0 >= e || e >= bounds for e in x[4:6:end]) || any(0.0 >= e || e >= bounds for e in x[5:6:end]) || any(0.0 >= e || e >= bounds for e in x[6:6:end])
         return Inf, Dict("Vs" => Inf, "As" => Inf, "Cs" => Inf, "Xs" => Inf, "OLs" => Inf, "PDGMs"  => nothing)
     end
-    n_mol = length(x) ÷ 6
     n_atoms_per_mol = size(template_centers)[2]
     flat_realization = MorphoMol.Utilities.get_flat_realization(x, template_centers)
     points = Vector{Vector{Float64}}([e for e in eachcol(reshape(flat_realization, (3, Int(length(flat_realization) / 3))))])
     pdgm = MorphoMol.Energies.get_persistence_diagram(points)
     pdgm = [pdgm[1], pdgm[2], pdgm[3]]
     measures = MorphoMol.Energies.get_geometric_measures_and_overlap_value(flat_realization, n_atoms_per_mol, radii, rs, overlap_jump, overlap_slope, delaunay_eps)
-    sum(measures .* [prefactors; [1.0]]) + MorphoMol.Energies.get_total_persistence(pdgm, persistence_weights), Dict("Vs" => measures[1], "As" => measures[2], "Cs" => measures[3], "Xs" => measures[4], "OLs" => measures[5], "PDGMs"  => pdgm)
+    sum(measures .* [prefactors; [1.0]]) + MorphoMol.Energies.get_total_persistence(pdgm, persistence_weights), Dict{String, Any}("Vs" => measures[1], "As" => measures[2], "Cs" => measures[3], "Xs" => measures[4], "OLs" => measures[5], "PDGMs"  => pdgm)
+end
+
+function solvation_free_energy_with_persistence_in_bounds_without_diagrams(x::Vector{Float64}, template_centers::Matrix{Float64}, radii::Vector{Float64}, rs::Float64, prefactors::AbstractVector, overlap_jump::Float64, overlap_slope::Float64, persistence_weights::Vector{Float64}, bounds::Float64, delaunay_eps::Float64)
+    if any(0.0 >= e || e >= bounds for e in x[4:6:end]) || any(0.0 >= e || e >= bounds for e in x[5:6:end]) || any(0.0 >= e || e >= bounds for e in x[6:6:end])
+        return Inf, Dict("Vs" => Inf, "As" => Inf, "Cs" => Inf, "Xs" => Inf, "OLs" => Inf, "PDGMs"  => nothing)
+    end
+    n_atoms_per_mol = size(template_centers)[2]
+    flat_realization = MorphoMol.Utilities.get_flat_realization(x, template_centers)
+    points = Vector{Vector{Float64}}([e for e in eachcol(reshape(flat_realization, (3, Int(length(flat_realization) / 3))))])
+    pdgm = MorphoMol.Energies.get_persistence_diagram(points)
+    pdgm = [pdgm[1], pdgm[2], pdgm[3]]
+    measures = MorphoMol.Energies.get_geometric_measures_and_overlap_value(flat_realization, n_atoms_per_mol, radii, rs, overlap_jump, overlap_slope, delaunay_eps)
+    sum(measures .* [prefactors; [1.0]]) + MorphoMol.Energies.get_total_persistence(pdgm, persistence_weights), Dict{String, Any}("Vs" => measures[1], "As" => measures[2], "Cs" => measures[3], "Xs" => measures[4], "OLs" => measures[5])
 end
