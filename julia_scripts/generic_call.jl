96d2ab28782bfcc15f868f0de97a8b4692af60b7
Ivan Spirandelli <ivan@spirandelli.de>
2025-01-30 16:30:59 -0800

    temp search implemented

--- julia_scripts/generic_call.jl
+++ julia_scripts/generic_call.jl
@@ -47,6 +47,54 @@ function generic_call(
         "simulation_time_minutes" => simulation_time_minutes,
     )
 
+    energy = MorphoMol.get_energy(input)
+    perturbation = MorphoMol.get_perturbation(input)
+
+    if isapprox(input["T"],0.0)
+        T = MorphoMol.get_initial_temperature(input; scaling = 0.0025)
+        input["T"] = T
+    end
+
+    search_Ts = [input["T"]]
+    search_αs = Vector{Float64}([])
+
+    for i in 1:T_search_runs
+        T = search_Ts[end]
+        output_search = Dict{String, Vector}(
+            "states" => Vector{Vector{Float64}}([]),
+            "Es" => Vector{Float64}([]), 
+            "Vs" => Vector{Float64}([]), 
+            "As" => Vector{Float64}([]), 
+            "Cs" => Vector{Float64}([]), 
+            "Xs" => Vector{Float64}([]),
+            "OLs" => Vector{Float64}([]),
+            "P0s" => Vector{Float64}([]),
+            "P1s" => Vector{Float64}([]),
+            "P2s" => Vector{Float64}([]),
+            "αs" => Vector{Float32}([]),
+        )
+        rwm = MorphoMol.Algorithms.RandomWalkMetropolis(energy, perturbation, 1/T)
+        MorphoMol.Algorithms.simulate!(rwm, MorphoMol.get_initial_state(n_mol, bnds), T_search_time, output_search);
+        α = output_search["αs"][end]
+        push!(search_αs, α)
+
+        if α > 0.24
+            push!(search_Ts, T * 0.5)
+        else
+            push!(search_Ts, T * 1.5)
+        end
+    end
+
+    if length(search_αs) > 1
+        println("Ts = $(search_Ts)")
+        println("αs = $(search_αs)")
+        input["T"] = search_Ts[argmin([abs(α - 0.24) for α in search_αs])]
+    end
+    β = 1.0 / input["T"]
+
+    println("T = $(input["T"])")
+
+    rwm = MorphoMol.Algorithms.RandomWalkMetropolis(energy, perturbation, β)
     output = Dict{String, Vector}(
         "states" => Vector{Vector{Float64}}([]),
         "Es" => Vector{Float64}([]), 
@@ -61,20 +109,12 @@ function generic_call(
         "αs" => Vector{Float32}([]),
     )
 
-    energy = MorphoMol.get_energy(input)
-    perturbation = MorphoMol.get_perturbation(input)
-
-    if isapprox(input["T"],0.0)
-        T = MorphoMol.get_initial_temperature(input; scaling = 0.003)
-        input["T"] = T
-    end
-    println("T = $(input["T"])")
-    β = 1.0/input["T"]
-
-    rwm = MorphoMol.Algorithms.RandomWalkMetropolis(energy, perturbation, β)
 
     MorphoMol.Algorithms.simulate!(rwm, deepcopy(x_init), simulation_time_minutes, output);
 
     mkpath("$(output_directory)")
     @save "$(output_directory)/$(name).jld2" input output
 end
+
+# function search_for_best_temperature()
+# end
